a7285fc5c266207aa4a10d7f3733a1ef91306cc0
Andrei Leanovich <andrei.leanovich@wsc-247-71b.icx.local>
2021-10-11 15:45:04 +0300

    Fixed run

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@
 #Run image
 run_value=$(cat ./secrets/run_value.txt)
 echo ${run_value.txt}
-docker run -p 8080:8080 -d ${run_value}/nodejs-application
+sudo docker run -p 8080:8080 -d ${run_value}/nodejs-application
