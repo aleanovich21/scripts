eec3c492ebd8fbd141b180e08434d0d3c9c6c56b
Andrei Leanovich <andrei.leanovich@wsc-247-71b.icx.local>
2021-10-11 11:48:05 +0300

    Changed variables

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 ##!/bin/bash
 
 #Build image
-source ./secret/repository_name.txt
+source ./secrets/repository_name.txt
 repository_name = cat "${repository_name.txt}"
 docker build -t ${repository_name}/nodejs-application -f Dockerfile .
--- push.sh
+++ push.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 #Push image to DockerHub
-source ./secret/repository_push_name.txt
+source ./secrets/repository_push_name.txt
 repository_push_name = cat "${repository_push_name.txt}"
 docker push ${repository_push_name}/nodejs-application
