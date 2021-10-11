2d2c2e123054703fc5a344cc2f37881d2c820dbc
Andrei Leanovich <andrei.leanovich@wsc-247-71b.icx.local>
2021-10-11 14:38:03 +0300

    Started parsing

--- build.sh
+++ build.sh
@@ -1,12 +1,6 @@
 ##!/bin/bash
 
 #Build image
-pwd
-ls -la
-cd ./secrets
-pwd
-ls -la
-cd ../
 repository_name=$(cat ./secrets/repository_name.txt)
 echo ${repository_name}
 docker build -t ${repository_name}/nodejs-application -f Dockerfile .
