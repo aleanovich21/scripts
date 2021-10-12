e41fbd31c81f43f31048d369e9f911d839f4bc41
Andrei Leanovich <andrei.leanovich@wsc-247-71b.icx.local>
2021-10-12 16:57:13 +0300

    Added version and tag

--- build.sh
+++ build.sh
@@ -1,7 +1,13 @@
 ##!/bin/bash
 
-#Getting meaning of variable from file pipeline_conf
+# Getting meaning of variable from file pipeline_conf
 repository_name=$(cat ./secrets/repository_name.txt)
 echo ${repository_name}
-#Build image
-docker build -t ${repository_name}/nodejs-application -f Dockerfile .
+# Getting name of image
+project_version=$(cat ./PROJECT_VERSION.txt)
+echo ${project_version}
+# Getting tag
+project_tag=$(cat ./PROJECT_TAG.txt)
+echo ${project_tag}
+# Build image
+docker build -t ${repository_name}/${project_version}:${project_tag} -f Dockerfile .
