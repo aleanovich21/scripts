24d25c86c79e1bd62b8a0163ae72d097a54ca47a
Andrei Leanovich <andrei.leanovich@wsc-247-71b.icx.local>
2021-10-13 13:36:09 +0300

    Added version

--- build.sh
+++ build.sh
@@ -6,8 +6,5 @@ echo ${repository_name}
 # Getting name of image
 project_version=$(cat ./PROJECT_VERSION.txt)
 echo ${project_version}
-# Getting tag
-project_tag=$(cat ./PROJECT_TAG.txt)
-echo ${project_tag}
 # Build image
-docker build -t ${repository_name}/${project_version}:${project_tag} -f Dockerfile .
+docker build -t ${repository_name}/${project_version} -f Dockerfile .
