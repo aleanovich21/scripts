8e0a81386bb8fffe1037d39cc0e9176383a6ad59
Andrei Leanovich <andrei.leanovich@wsc-247-71b.icx.local>
2021-10-14 11:05:22 +0300

    Added folder

--- push.sh
+++ push.sh
@@ -2,7 +2,7 @@
 
 #Push image to DockerHub
 repository_push_name=$(cat ./secrets/repository_push_name.txt)
-echo ${repository_push_name.txt}
+echo ${repository_push_name}
 # Getting name of image
 project_version=$(cat ./PROJECT_VERSION.txt)
 echo ${project_version}
